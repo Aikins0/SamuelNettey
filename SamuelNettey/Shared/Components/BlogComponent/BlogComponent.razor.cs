5562cfe6f3a8590373c8896e708265ed3d3ce2f0
Richard Amo Mante <richard.mante@scynett.com>
2024-01-31 12:54:33 +0000

    build component for blogs

---
+++ SamuelNettey/Shared/Components/BlogComponent/BlogComponent.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SamuelNettey.Shared.Components.BlogComponent
+{
+    public partial class BlogComponent
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public string Description { get; set; }
+
+        [Parameter]
+        public string Author { get; set; }
+
+        [Parameter]
+        public string Img { get; set; }
+
+        [Parameter]
+        public string Date { get; set; }
+
+        [Parameter]
+        public int Comments { get; set; }
+
+        [Parameter]
+        public string[] Technologies { get; set; }
+    }
+}
